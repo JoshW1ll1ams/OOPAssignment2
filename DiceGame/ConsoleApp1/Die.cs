a18006ad4bbe235294453991992998546b5a880a
Josh Williams <jlw1998@protonmail.com>
2024-04-26 15:40:23 +0100

    added base files

---
+++ DiceGame/ConsoleApp1/Die.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Die
+    {
+        // Current value variable will hold the result of the Roll method, It is public so we can access it outside of the class
+        public int CurrentValue = 0;
+
+        // We create an instance of the random class so we can use the Next method for our random die roll value
+        private static Random random = new Random();
+
+        // Roll method called by the Game class on each instance of Die object it creates
+        public int Roll()
+        {
+            // Set current value to a random integer between 1 and 6
+            CurrentValue = random.Next(1, 7);
+
+            // Return the current value variable
+            return CurrentValue;
+        }
+    }
+
+}
+
---
+++ DiceGame/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Die Die1 = new Die();
+   
+            Console.WriteLine(Die1.Roll());
+        }
+    }
+}
